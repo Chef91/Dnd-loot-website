5dfbd0828b205496ec9cf9ad1aadc396080b5858
Chef91 <chefgraham1991@gmail.com>
2026-07-01 16:43:33 -0400

    commit

---
+++ javascript/index/main.js
@@ -0,0 +1,3 @@
+function selectProfile (profiles) {
+  
+}
---
+++ javascript/index/profiles.js
@@ -0,0 +1,42 @@
+export const profiles = [
+  {
+    image: "/images/homepage/team/chef.wepb", // fix this
+    name: "Chef",
+    age: 35,
+    coding: "2 years",
+    ttrpg: "5 years",
+    favorite: "Dungeons & Dragons"
+  },
+  {
+    image: "/images/homepage/team/1.img", // fix this
+    name: "Coder 2",
+    age: 20,
+    coding: "1 years",
+    ttrpg: "1 years",
+    favorite: "Dungeons & Dragons"
+  },
+  {
+    image: "/images/homepage/team/1.img", // fix this
+    name: "Coder 3",
+    age: 20,
+    coding: "1 years",
+    ttrpg: "1 years",
+    favorite: "Dungeons & Dragons"
+  },
+  {
+    image: "/images/homepage/team/1.img", // fix this
+    name: "Coder 4",
+    age: 20,
+    coding: "1 years",
+    ttrpg: "1 years",
+    favorite: "Dungeons & Dragons"
+  },
+  {
+    image: "/images/homepage/team/1.img", // fix this
+    name: "Coder 5",
+    age: 20,
+    coding: "1 years",
+    ttrpg: "1 years",
+    favorite: "Dungeons & Dragons"
+  },
+]
